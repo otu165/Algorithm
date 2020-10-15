d1aaa4af50e746b602595c0f314414feb7fd8edb
otu165 <micheu35@gmail.com>
2020-10-15 16:59:34 +0900

    Baekjoon_2739

---
+++ src/bronze/BJ_2739.java
@@ -0,0 +1,20 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //입력
+        int n = Integer.parseInt(br.readLine());
+
+        //풀이
+        for (int i = 1; i < 10; i++) {
+            //출력
+            System.out.printf("%d * %d = %d\n", n, i, n * i);
+        }
+    }
+}
