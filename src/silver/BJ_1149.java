bebc52c536554f6f0c6920ab2849a9978d48fdd4
otu165 <micheu35@gmail.com>
2020-10-16 21:02:42 +0900

    Baekjoon_1149

--- src/silver/BJ_1149.java
+++ src/silver/BJ_1149.java
@@ -6,62 +6,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BJ_1149 {
-    public static int min = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         //입력
         int n = Integer.parseInt(br.readLine());
-
-        int expense[][] =  new int[n][3];
-        for (int i = 0; i < expense.length; i++) {
+        int matrix[][] = new int[n][3];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < 3; j++) {
-                expense[i][j] = Integer.parseInt(st.nextToken());
+                matrix[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         //풀이
-        //기준이 되는 값 선정
-        int flagIndex = 0, flagValue = expense[0][0];
-        for (int i = 1; i < 3; i++) {
-            if(expense[0][i] < flagValue) {
-                flagValue = expense[0][i];
-                flagIndex = i;
-            }
-        }
-        min = flagValue;
+        int valueMatrix[][] = new int[n][3];
 
+        valueMatrix[0][0] = matrix[0][0];
+        valueMatrix[0][1] = matrix[0][1];
+        valueMatrix[0][2] = matrix[0][2];
 
         for (int i = 1; i < n; i++) {
-            //min expense 찾기
-            boolean flag = true;
-
-            for (int j = 0; j < 3; j++) {
-                if(j != flagIndex) {
-                    if (flag) { //첫 실행
-                        flagValue = expense[i][j];
-                        flag = false;
-                    }
-                    else {
-                        if (flagValue < expense[i][j]) {
-                            flagIndex = (flagIndex == 0) ? 1 : 0;
-                            min += flagValue;
-                        }
-                        else {
-                            min += expense[i][j];
-                            flagIndex = j;
-                        }
-                        break;
-                    }
-                }
-            }
+            valueMatrix[i][0] = matrix[i][0] + Math.min(valueMatrix[i - 1][1], valueMatrix[i - 1][2]);
+            valueMatrix[i][1] = matrix[i][1] + Math.min(valueMatrix[i - 1][0], valueMatrix[i - 1][2]);
+            valueMatrix[i][2] = matrix[i][2] + Math.min(valueMatrix[i - 1][0], valueMatrix[i - 1][1]);
         }
 
         //출력
-        System.out.println(min);
+        int minValue = Math.min(Math.min(valueMatrix[n-1][0], valueMatrix[n-1][1]), valueMatrix[n-1][2]);
+        System.out.println(minValue);
    }
 }
