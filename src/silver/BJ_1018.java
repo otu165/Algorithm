4c3bca7909dbefd716dd7e571abd5a77fc66afb1
otu165 <micheu35@gmail.com>
2020-10-17 15:13:21 +0900

    Baekjoon_1018 ~ing

--- src/silver/BJ_1018.java
+++ src/silver/BJ_1018.java
@@ -14,68 +14,96 @@ public class BJ_1018 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        String[][] matrix = new String[n][m];
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-
-            for (int j = 0; j < m; j++) {
-                matrix[i][j] = String.valueOf(s.charAt(i));
-            }
-        }
-
         //풀이
-        String[][] comparisonA = new String[8][8]; //W시작
-        String[][] comparisonB = new String[8][8]; //B시작
+        String[][] matrix = new String[n][m];
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if(j % 2 == 0) {
-                    comparisonA[i][j] = "W";
-                    comparisonB[i][j] = "B";
-                }
-                else {
-                    comparisonA[i][j] = "B";
-                    comparisonB[i][j] = "W";
-                }
-            }
-        }
+        int[][] comparisonA = new int[n][m]; //WBWB + BWBW
+        int[][] comparisonB = new int[n][m]; //BWBW + WBWB
+        initializeComparison(br, comparisonA, comparisonB);
 
-        int[][] scoreA = new int[n - 7][m - 7];
-        int[][] scoreB = new int[n - 7][m - 7];
         int min = Integer.MAX_VALUE;
 
-        for (int i = 0; i < scoreA.length; i++) {
-            int countA = 0, countB = 0;
-            int indexX = 0, indexY = 0;
+        for (int i = 0; i < n - 7; i++) {
+            for (int j = 0; j < m - 7; j++) {
 
-            for (int j = 0; j < scoreA[i].length; j++) {
-                //체스판 검사
-                for (int k = i; k < 7 + i; k++) {
-                    for (int l = j; l < 7 + j; l++) {
-                        //comparisonA와 대조
-                        if (!matrix[k][l].equals(comparisonA[indexX][indexY])) {
-                            countA++;
-                        }
-
-                        //comparisonB와 대조
-                        if (!matrix[k][l].equals(comparisonB[indexX][indexY++])) {
-                            countB++;
-                        }
-                    }
-                    indexX++;
-                    indexY = 0;
+                //체스판의 합
+                if (getSum(comparisonA, comparisonB, i, j) < min) {
+                    min = getSum(comparisonA, comparisonB, i, j);
                 }
-
-                //min값 초기화
-                min = countA < min ? countA : min;
-                min = countB < min ? countB : min;
-
-                scoreA[i][j] = countA;
-                scoreB[i][j] = countB;
             }
         }
 
         //출력
         System.out.println(min);
+    }
+
+    public static void initializeComparison(BufferedReader br, int[][] comparisonA, int[][] comparisonB) throws IOException{
+        for (int i = 0; i < comparisonA.length; i++) {
+            String s = br.readLine();
+
+            for (int j = 0; j < comparisonA[i].length; j++) {
+                //A : 같으면 0, 다르면 1
+                //B : 같으면 0, 다르면 1
+                String word = String.valueOf(s.charAt(j));
+
+                if (i % 2 == 0) {
+                    if (j % 2 == 0) { //짝 + 짝
+                        if(word.equals("W")) {
+                            comparisonA[i][j] = 0;
+                            comparisonB[i][j] = 1;
+                        }
+                        else {
+                            comparisonA[i][j] = 1;
+                            comparisonB[i][j] = 0;
+                        }
+                    }
+                    else { //짝 + 홀
+                        if (word.equals("B")) {
+                            comparisonA[i][j] = 0;
+                            comparisonB[i][j] = 1;
+                        }
+                        else {
+                            comparisonA[i][j] = 1;
+                            comparisonB[i][j] = 0;
+                        }
+                    }
+                }
+                else {
+                    if (j % 2 == 0) { //홀 + 짝
+                        if (word.equals("B")) {
+                            comparisonA[i][j] = 0;
+                            comparisonB[i][j] = 1;
+                        }
+                        else {
+                            comparisonA[i][j] = 1;
+                            comparisonB[i][j] = 0;
+                        }
+                    }
+                    else { //홀 + 홀
+                        if(word.equals("W")) {
+                            comparisonA[i][j] = 0;
+                            comparisonB[i][j] = 1;
+                        }
+                        else {
+                            comparisonA[i][j] = 1;
+                            comparisonB[i][j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static int getSum(int[][] comparisonA, int[][] comparisonB, int x, int y) {
+        int sumA = 0, sumB = 64;
+
+        for (int i = x; i < 8 + x; i++) {
+            for (int j = y; j < 8 + y; j++) {
+                sumA += comparisonA[i][j];
+                sumB -= comparisonB[i][j];
+            }
+        }
+
+        return sumA < sumB ? sumA : sumB;
     }
 }
