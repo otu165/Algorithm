c40110f4ca4381aba2319ca07841823a8ee3adbe
otu165 <micheu35@gmail.com>
2020-10-29 17:12:49 +0900

    Baekjoon_1436

---
+++ src/silver/BJ_1436.java
@@ -0,0 +1,25 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력
+        int N = Integer.parseInt(br.readLine());
+
+        int result = 665;
+        while (N > 0) {
+            result++;
+
+            String s = String.valueOf(result);
+            if(s.contains("666")) {
+                N--;
+            }
+        }
+        System.out.println(result);
+    }
+}
