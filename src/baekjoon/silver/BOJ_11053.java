9a323c5a782c92454bec6fecd6bded8b2b45f6be
otu165 <micheu35@gmail.com>
2020-11-18 15:14:18 +0900

    Baekjoon_11053

--- src/baekjoon/silver/BOJ_11053.java
+++ src/baekjoon/silver/BOJ_11053.java
@@ -1,7 +1,6 @@
 package baekjoon.silver;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_11053 {
@@ -18,21 +17,25 @@ public class BOJ_11053 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < N; i++) {
-            int total = 1;
-            int max = arr[i];
+        // DP 이용
+        count[0] = 1;
 
-            for (int j = i + 1; j < N; j++) {
-                if(max < arr[j]) {
-                    max = arr[j];
-                    total++;
+        for (int i = 1; i < N; i++) {
+            count[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if(arr[j] < arr[i] && count[i] <= count[j]) {
+                    count[i] = count[j] + 1;
                 }
             }
-
-            count[i] = total;
         }
 
-        bw.write(Arrays.stream(count).max().getAsInt() + "");
+        // 최댓값 찾기
+        int max = 0;
+        for(int n : count)
+            max = Math.max(max, n);
+
+        bw.write(max + "");
 
         bw.flush();
         bw.close();
