3461b4b76f8a885a8297572a0315b7d0dbeec6d7
Juyeon Yoon <otu165@naver.com>
2021-04-12 21:16:54 +0900

    delete directories

--- python/permutation/BOJ_15649.py
+++
@@ -1,9 +0,0 @@
-from itertools import permutations
-
-N, M = map(int, input().split())  # 1 <= M <= N <= 8
-
-n_list = [x for x in range(1, N + 1)]
-result = list(permutations(n_list, M))
-
-for i in result:
-    print(*i)
