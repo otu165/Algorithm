10f6feff1f0790836c53d7cb2e57c2e35b8682ad
Juyeon <otu165@naver.com>
2021-07-03 02:12:00 +0900

    PG_12899

---
+++ python/PG_12899.py
@@ -0,0 +1,13 @@
+# 124 나라의 숫자
+
+def solution(n):
+    answer = ''
+    while n > 0:
+        if n % 3 == 0:
+            answer += str(4)
+            n = n // 3 - 1
+        else:
+            answer += str(n % 3)
+            n = n // 3
+
+    return answer[::-1]  # 역순 리턴
